9d40edcda02f7cfbcdeabd25e51ef3cd5b2acb40
tlev159 <t.levente1598@gmail.com>
2021-03-13 07:13:40 +0100

    week05d03 count words from a list starting with 'a' or 'A'

---
+++ src/main/java/week05d3/ListCounter.java
@@ -0,0 +1,21 @@
+package week05d3;
+
+import java.util.List;
+
+public class ListCounter {
+
+  private static final String START_LETTER = "a";
+
+  public int countStringsStartsWithA(List<String> words) {
+    if (words == null || words.size() == 0) {
+      return 0;
+    }
+    int result = 0;
+    for (String str:words) {
+      if (str.toLowerCase().startsWith(START_LETTER)) {
+        result++;
+      }
+    }
+    return result;
+  }
+}
---
+++ src/test/java/week05d3/ListCounterTest.java
@@ -0,0 +1,44 @@
+package week05d3;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListCounterTest {
+
+  @Test
+  void testCountStringsStartsWithA() {
+
+    ListCounter listCounter = new ListCounter();
+
+    List<String> temp = new ArrayList<>(Arrays.asList("alma", "körte", "Aladár", "kiskacsa", "Aladdin"));
+
+    assertEquals(3, listCounter.countStringsStartsWithA(temp));
+  }
+
+  @Test
+  void testCountStringsStartsWithAIfResultNull() {
+
+    ListCounter listCounter = new ListCounter();
+
+    List<String> temp = new ArrayList<>(Arrays.asList("zöldalma", "körte", "Haladár", "kiskacsa", "kaladdin"));
+
+    assertEquals(0, listCounter.countStringsStartsWithA(temp));
+  }
+
+  @Test
+  void testIfListEmpty() {
+
+    ListCounter listCounter = new ListCounter();
+
+    List<String> temp = null;
+
+    assertEquals(0, listCounter.countStringsStartsWithA(temp));
+  }
+
+
+}
